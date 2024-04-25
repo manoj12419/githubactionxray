80a80e27bfed3fffb9c384ca92e3356c6481ee78
unknown <manojnarikadapu@gmail.com>
2024-04-25 15:01:20 -0400

    add file

--- .github/workflows/test/uploadtest.py
+++ .github/workflows/test/uploadtest.py
@@ -10,6 +10,7 @@ def authenticate(client_id, client_secret):
     try:
         response = requests.post(uri, headers=headers, data=body)
         response.raise_for_status()
+        print(response.text)
         return response.text  # Assuming the response is a plain string
     except requests.exceptions.RequestException as e:
         print(f"Error during authentication: {e}")
@@ -27,11 +28,18 @@ def import_execution_junit(token, test_id, file_path):
         with open(file_path, "rb") as file:
             response = requests.post(uri, headers=headers, files={"file": file})
             response.raise_for_status()
-            print(response.text)
+            print("Import successful.")
+            print(response.text)  # Print API response for debugging
     except requests.exceptions.RequestException as e:
         print(f"Error during import: {e}")
+        if response.status_code == 400:
+            print("Bad request. Check the request parameters and format.")
+        else:
+            print("Unexpected error occurred.")
+
 
 def main():
+    print("Before if Usage: python uploadtest.py <client_id> <client_secret> <file_path> <test_id> <test_exec_id>")
     if len(sys.argv) != 6:
         print("Usage: python uploadtest.py <client_id> <client_secret> <file_path> <test_id> <test_exec_id>")
         sys.exit(1)
