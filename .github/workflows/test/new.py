e1f99e72a539ecfcdbbf4b07e705cd2decfb9493
unknown <manojnarikadapu@gmail.com>
2024-04-25 18:29:36 -0400

    add file

--- .github/workflows/test/new.py
+++ .github/workflows/test/new.py
@@ -23,18 +23,17 @@ def upload_junit_results():
     uri_authenticate = "https://xray.cloud.getxray.app/api/v1/authenticate"
     headers_authenticate = {"Content-Type": "application/json"}
     body_authenticate = json.dumps({"client_id": args.client_id, "client_secret": args.client_secret})
-
+   
     response_authenticate = requests.post(uri_authenticate, headers=headers_authenticate, data=body_authenticate)
-    response_authenticate.raise_for_status()
-    print("Authentication response:")
+    print(f"Authorization: Bearer {response_authenticate}")
+    print(f"Authorization: Bearer {response_authenticate.text}")
     print(response_authenticate.text)
     token = response_authenticate.text  # Assuming the response is a plain string
-    bT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnQiOiI3MTc5OGI0YS1jNzdmLTM5NTctYjdhMC1kMjVlNmE3OWI0MDYiLCJhY2NvdW50SWQiOiI2M2I3NGI0NzZmMDY4ZWZlYzhmOGUxOWUiLCJpc1hlYSI6ZmFsc2UsImlhdCI6MTcxNDA4MzU1MiwiZXhwIjoxNzE0MTY5OTUyLCJhdWQiOiJDMkREQzAzRUI4MDE0ODVFOTNBOEExQkJGMjlBNjBGOCIsImlzcyI6ImNvbS54cGFuZGl0LnBsdWdpbnMueHJheSIsInN1YiI6IkMyRERDMDNFQjgwMTQ4NUU5M0E4QTFCQkYyOUE2MEY4In0.c4KrPOGbfPnSy59gCamekaNg8nEkLSfP0I5IkZEk9AY"
     #uri_import_execution = f"https://xray.cloud.getxray.app/api/v1/import/execution/junit?projectKey=YAK&testPlanKey={args.test_id}"
-    print(f"Authorization: Bearer {bT}")
+    
     uri_import_execution = "https://xray.cloud.getxray.app/api/v1/import/execution/junit?projectKey=YAK&testPlanKey=YAK-4"
     headers_import_execution = {
-        "Authorization": f"Bearer {bT}",
+        "Authorization": f"Bearer {response_authenticate}",
         "Content-Type": "application/xml",
     }
 
