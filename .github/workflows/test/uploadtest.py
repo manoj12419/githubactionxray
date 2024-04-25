0be6ac55060e6e76b809d5bcb9167a9a5b1f6515
unknown <manojnarikadapu@gmail.com>
2024-04-25 15:28:37 -0400

    add file

--- .github/workflows/test/uploadtest.py
+++ .github/workflows/test/uploadtest.py
@@ -9,18 +9,24 @@ def authenticate(client_id, client_secret):
 
     response = requests.post(uri, headers=headers, data=body)
     response.raise_for_status()
+    print("Authentication response:")
     print(response.text)
     return response.text  # Assuming the response is a plain string
 
-def import_execution_junit(token, test_id, file_path):
+def import_execution_junit(token, test_id, file_content):
     uri = f"https://xray.cloud.getxray.app/api/v1/import/execution/junit?projectKey=YAK&testPlanKey={test_id}"
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/xml",
     }
 
-    # Assuming file_path is the content of the XML file
-    response = requests.post(uri, headers=headers, data=file_path)
+    print("Import request:")
+    print(f"URI: {uri}")
+    print(f"Headers: {headers}")
+    print("File content:")
+    print(file_content)
+
+    response = requests.post(uri, headers=headers, data=file_content)
     response.raise_for_status()
     print("Import successful.")
     print(response.text)  # Print API response for debugging
@@ -31,7 +37,7 @@ print(f"Testplan id: {test_id}")
 print(f"Test exec id: {test_exec_id}")
 print(f"Received client_id: {client_id}")
 print(f"Received client_secret: {client_secret}")
-print(f"Received filePath: {file_content}")
+print(f"Received file content: {file_content}")
 
 token = authenticate(client_id, client_secret)
 if token:
