4d0b422d92f63063983e17a9c413d336879259f8
unknown <manojnarikadapu@gmail.com>
2024-04-26 00:25:34 -0400

    add file

--- .github/workflows/test/new.py
+++ .github/workflows/test/new.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 import json
+import xmltodict
+import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Upload JUnit results to Xray.")
@@ -10,6 +12,13 @@ def parse_args():
     parser.add_argument("test_id", help="Test ID or test plan key")
     return parser.parse_args()
 
+def convert_xml_to_json(xml_file):
+    with open(xml_file, 'r') as f:
+        xml_data = f.read()
+        json_data = xmltodict.parse(xml_data)
+        with open('results.json', 'w') as json_file:
+            json.dump(json_data, json_file)
+
 def upload_junit_results(args):
     print(f"Test ID or test plan key: {args.test_id}")
     print(f"Client ID: {args.client_id}")
@@ -17,13 +26,12 @@ def upload_junit_results(args):
     print(f"File path: {args.file_path}")
 
     url = "https://xray.cloud.getxray.app/api/v1/authenticate"
-    #headers_authenticate = {"Content-Type": "application/json"}
     data = {
-        'client_id':args.client_id,
+        'client_id': args.client_id,
         'client_secret': args.client_secret,
-        'grant_type' : 'client_credentials'
+        'grant_type': 'client_credentials'
     }
-    request= requests.post(url,data=data)
+    request = requests.post(url, data=data)
     print(request)
     print(request.text)
     header = {
@@ -31,12 +39,11 @@ def upload_junit_results(args):
         "Content-Type": "application/xml",
     }
 
+    convert_xml_to_json(args.file_path)  # Convert XML to JSON
+    files = {'file': open('results.json', 'rb')}  # Use the JSON file for upload
     url_import_execution = f"https://xray.cloud.getxray.app/api/v1/import/execution/junit?projectKey=YAK&testPlanKey={args.test_id}"
-    files = {'file': open(args.file_path, 'rb')}
     response_import_execution = requests.post(url_import_execution, headers=header, files=files)
     print(response_import_execution.text)
-
-   
 
 if __name__ == "__main__":
     args = parse_args()
