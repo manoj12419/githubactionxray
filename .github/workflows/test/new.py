e81163b72819acc67c0e83703a25fd84402d5b4d
unknown <manojnarikadapu@gmail.com>
2024-04-26 01:02:13 -0400

    add file

--- .github/workflows/test/new.py
+++ .github/workflows/test/new.py
@@ -25,12 +25,13 @@ def upload_junit_results(args):
     auth_response = requests.post(auth_url, data=auth_data)
 
     print(f"Authorization token: {auth_response.text}")
+    bearertoken=f"Bearer {auth_response.text}"
 
     # Step 2: Upload JUnit results using the obtained token
     import_url = f"https://xray.cloud.getxray.app/api/v1/import/execution/junit?projectKey=YAK&testPlanKey={args.test_id}"
     headers = {
-        #"Authorization": f"Bearer {auth_response.text}",
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnQiOiI3MTc5OGI0YS1jNzdmLTM5NTctYjdhMC1kMjVlNmE3OWI0MDYiLCJhY2NvdW50SWQiOiI2M2I3NGI0NzZmMDY4ZWZlYzhmOGUxOWUiLCJpc1hlYSI6ZmFsc2UsImlhdCI6MTcxNDEwNzQ2NiwiZXhwIjoxNzE0MTkzODY2LCJhdWQiOiJDMkREQzAzRUI4MDE0ODVFOTNBOEExQkJGMjlBNjBGOCIsImlzcyI6ImNvbS54cGFuZGl0LnBsdWdpbnMueHJheSIsInN1YiI6IkMyRERDMDNFQjgwMTQ4NUU5M0E4QTFCQkYyOUE2MEY4In0.UhSo_VyBY3twwPlnH3UqSOmbaV3bpa5Zl5lnUzWHQUw',
+        "Authorization": bearertoken,
+        #'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnQiOiI3MTc5OGI0YS1jNzdmLTM5NTctYjdhMC1kMjVlNmE3OWI0MDYiLCJhY2NvdW50SWQiOiI2M2I3NGI0NzZmMDY4ZWZlYzhmOGUxOWUiLCJpc1hlYSI6ZmFsc2UsImlhdCI6MTcxNDEwNzQ2NiwiZXhwIjoxNzE0MTkzODY2LCJhdWQiOiJDMkREQzAzRUI4MDE0ODVFOTNBOEExQkJGMjlBNjBGOCIsImlzcyI6ImNvbS54cGFuZGl0LnBsdWdpbnMueHJheSIsInN1YiI6IkMyRERDMDNFQjgwMTQ4NUU5M0E4QTFCQkYyOUE2MEY4In0.UhSo_VyBY3twwPlnH3UqSOmbaV3bpa5Zl5lnUzWHQUw',
         "Content-Type": "application/xml",
     }
     print(headers)
